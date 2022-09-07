bf99ce20d052dfcb1c822b918118333ee98219b3
rebzzel <rebzzel@gmail.com>
2022-09-08 01:25:04 +0300

    Fix tabulation

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 const illuminate = (element) => {
-	element.classList.add('illuminate');
-	setTimeout(() => element.classList.remove('illuminate'), 1000);
+    element.classList.add('illuminate');
+    setTimeout(() => element.classList.remove('illuminate'), 1000);
 };
 
 
 const illuminateById = (id) => {
-	const element = document.getElementById(id);
+    const element = document.getElementById(id);
 
-	if (!element) {
-		throw Error(`Element with ${id} not found!`);
-	}
+    if (!element) {
+        throw Error(`Element with ${id} not found!`);
+    }
 
-	illuminate(element);
+    illuminate(element);
 };
 
 
 window.onload = () => {
-	const linksElements = document.getElementsByTagName('a');
+    const linksElements = document.getElementsByTagName('a');
 
-	for (const linkElement of linksElements) {
-		if (!linkElement.hash) continue;
-		const sectionId = linkElement.hash.substring(1);
-		linkElement.onclick = () => illuminateById(sectionId);
-	}
+    for (const linkElement of linksElements) {
+        if (!linkElement.hash) continue;
+        const sectionId = linkElement.hash.substring(1);
+        linkElement.onclick = () => illuminateById(sectionId);
+    }
 };
 
 
 window.onhashchange = () => {
-	const sectionId = window.location.hash.substring(1);
-	illuminateById(sectionId);
+    const sectionId = window.location.hash.substring(1);
+    illuminateById(sectionId);
 };
