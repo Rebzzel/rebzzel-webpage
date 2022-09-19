0d137f1f6a6672908b071f42c10aaa96fb5555e4
rebzzel <rebzzel@gmail.com>
2022-09-19 18:36:31 +0300

    Add footer component

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+function Component() {
+    return (
+        <div className="text-center">
+            <text>&copy; 2022 Rebzzel. All rights reserved.</text>
+        </div>
+    )
+}
+
+export default Component
