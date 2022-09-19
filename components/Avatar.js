7f77206e879924b1e2aff627d48cb96de9e3c1dc
rebzzel <rebzzel@gmail.com>
2022-09-19 19:00:56 +0300

    Add avatar component

---
+++ components/Avatar.js
@@ -0,0 +1,12 @@
+function Component({ width = 128, height = 128 }) {
+    return (
+        <img
+            className='rounded-full border border-slate-300'
+            width={width}
+            height={height}
+            src='https://avatars.githubusercontent.com/u/30433331?v=4'
+        />
+    )
+}
+
+export default Component
