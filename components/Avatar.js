5d22189751895b4b052b83b94a748035e62011c2
rebzzel <rebzzel@gmail.com>
2022-09-21 12:03:02 +0300

    Use next/image instead img, and add size configuration for avatar component

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,13 @@
+import Image from 'next/future/image'
+
 function Component({ width = 128, height = 128 }) {
     return (
-        <img
-            className='rounded-full border border-slate-300'
+        <Image
+            className="rounded-full border border-slate-300"
             width={width}
             height={height}
-            src='https://avatars.githubusercontent.com/u/30433331?v=4'
+            src="https://avatars.githubusercontent.com/u/30433331?v=4"
+            alt=""
         />
     )
 }
