105630c9bbea3fd48cf86f87b4dcc54f3bf8031e
rebzzel <rebzzel@gmail.com>
2022-09-24 15:01:43 +0300

    Add _document.js

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+function Document() {
+    return (
+        <Html>
+            <Head>
+                <title>Kirill Paltsev's Webpage</title>
+            </Head>
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </Html>
+    )
+}
+
+export default Document
