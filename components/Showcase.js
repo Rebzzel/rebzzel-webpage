766b03585ee7b36dc188369fb95c07aafbf0ec2f
rebzzel <rebzzel@gmail.com>
2022-09-24 14:45:31 +0300

    Add showcase component

---
+++ components/Showcase.js
@@ -0,0 +1,29 @@
+import clsx from 'clsx'
+import Image from 'next/image'
+import Interactive from './future/Interactive.js'
+
+function Component({ className, href, imageSrc, children }) {
+    return (
+        <Interactive className="duration-300">
+            <a className="flex flex-col items-center" href={href}>
+                <Image
+                    layout="intrinsic"
+                    src={imageSrc}
+                    width={256}
+                    height={120}
+                    objectFit="cover"
+                    className="rounded-md"
+                />
+                <div className={clsx(
+                    'w-full',
+                    'p-1',
+                    className,
+                )}>
+                    {children}
+                </div>
+            </a>
+        </Interactive>
+    )
+}
+
+export default Component
