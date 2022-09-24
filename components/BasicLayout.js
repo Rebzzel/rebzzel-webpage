caad2f8f7c76d57b3e6f9fa47c153f09ce40cca1
rebzzel <rebzzel@gmail.com>
2022-09-24 14:47:06 +0300

    Right layout construction

--- components/BasicLayout.js
+++ components/BasicLayout.js
@@ -1,12 +1,24 @@
+import clsx from 'clsx'
+
 import Navbar from './Navbar.js'
 import Footer from './Footer.js'
 
-function Component({ children }) {
+function Component({ children, ...props }) {
+    const childrenContainerClass = clsx(
+        'pt-16',
+        'px-2',
+        'md:px-0',
+
+        'container',
+        'mx-auto',
+        'md:max-w-xl',
+    )
+
     return (
-        <div>
+        <div className="flex flex-col min-h-screen justify-between">
             <Navbar/>
-            <div className="pt-16 container mx-auto md:max-w-xl">
-                {children}
+            <div className={childrenContainerClass}>
+                { children }
             </div>
             <Footer/>
         </div>
