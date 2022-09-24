cd036299ee4a00e2392b3521bd8c14bd700700b4
rebzzel <rebzzel@gmail.com>
2022-09-24 14:49:46 +0300

    Add blog page sketch

---
+++ pages/blog/index.js
@@ -0,0 +1,20 @@
+import PassportLayout from '../../components/PassportLayout.js'
+import SwapContainer from '../../components/future/SwapContainer.js'
+
+function Page() {
+    return (
+        <PassportLayout>
+            <SwapContainer>
+                <p className="p-2 py-6 text-2xl text-center">
+                    🏗️
+                    <br/>
+                    We're building something here...
+                    <br/>
+                    See you later!
+                </p>
+            </SwapContainer>
+        </PassportLayout>
+    )
+}
+
+export default Page
